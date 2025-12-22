2e875518ecb4b088685d73cc41c0a1a1e02aceb2
Candice Guitton <candice@soundcheckcapital.com>
2025-12-22 12:59:50 +0100

    Add favicon and update site title to Soundcheck Capital

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import './globals.css'
 import Header from '../components/Header'
 
 export const metadata = {
-  title: 'Soundcheck Showcase',
+  title: 'Soundcheck Capital',
   description: 'Site vitrine Soundcheck',
 }
 
