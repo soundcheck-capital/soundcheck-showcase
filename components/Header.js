e7b5a334a6c7b2f51d20aa18db2c758c6c60beb4
Candice Guitton <candice@soundcheckcapital.com>
2026-01-15 08:59:31 +0000

    fix logo

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,6 @@
 'use client'
 
 import Link from 'next/link'
-import Image from 'next/image'
 import './Header.css'
 
 export default function Header() {
@@ -17,13 +16,10 @@ export default function Header() {
     <header className="header">
       <div className="header-container">
         <Link href="/" className="logo">
-          <Image 
+          <img 
             src="/images/clean_logo.svg" 
             alt="Soundcheck Capital" 
-            width={150}
-            height={40}
             className="logo-image"
-            priority
           />
         </Link>
         <nav className="nav">
